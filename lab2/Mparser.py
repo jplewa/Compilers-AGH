4cb4ae73cca6ed1b7bbeb549ccf42d4fa4feb548
Julia Plewa <juliadplewa@gmail.com>
2019-04-03 17:25:52 +0200

    Refactor lab2

--- lab2/Mparser.py
+++ lab2/Mparser.py
@@ -1,17 +1,20 @@
+import lab1.scanner as scanner
 import ply.yacc as yacc
 import sys
 sys.path.append("..")
-import lab1.scanner as scanner
 
 
-tokens = scanner.tokens
+tokens = tuple(set(scanner.tokens) - set(("COMMENT",)))
 
 precedence = (
-    ("left", 'ADD', 'SUB'),
-    ("left", 'DIV', 'MUL'),
-    ("left", '='),
-    ("nonassoc", 'NO_ELSE', 'NO_INDEX'),
-    ("nonassoc", 'ELSE', '[')
+    ("nonassoc", 'IF'),
+    ("nonassoc", 'ELSE'),
+    ("right", '='),
+    ("nonassoc", "ADDASSIGN", "SUBASSIGN", "MULASSIGN", "DIVASSIGN"),
+    ("nonassoc", 'LT', 'GT', "LEQ", "GEQ", "EQ", "NEQ"),
+    ("left", 'ADD', 'SUB', 'DOTADD', 'DOTSUB'),
+    ("left",  'MUL', 'DIV', "DOTMUL", "DOTDIV"),
+    ("right", 'NEGATIVE'),
 )
 
 
@@ -33,202 +36,131 @@ def p_instructions_opt(p):
     """
 
 
-def p_comment_opt(p):
-    """ comment_opt : COMMENT
-                    |
-    """
-
-
 def p_instructions(p):
-    """ instructions : instructions single_instruction comment_opt
-                     | single_instruction comment_opt
-    """
-
-
-def p_single_instruction(p):
-    """ single_instruction : BREAK ';'
-                           | CONTINUE ';'
-                           | PRINT element_list ';'
-                           | RETURN instruction ';'
-                           | flow
-                           | block
-                           | instruction ';'
-    """
-
-
-def p_element_list(p):
-    """ element_list : instruction ',' element_list
+    """ instructions : instructions instruction
                      | instruction
     """
 
 
-def p_flow(p):
-    """ flow : while_loop
-             | for_loop
-             | if
-    """
-
-
 def p_instruction(p):
-    """ instruction : operand
-                    | CONDITION
+    """ instruction : return
+                    | break
+                    | continue
+                    | print
+    """
+
+def p_assignment_instruction(p):
+    """ instruction : variable '=' expression ';' """
+
+
+def p_arithmetic_assignment_instruction(p):
+    """ instruction : variable ADDASSIGN expression ';'
+                    | variable SUBASSIGN expression ';'
+                    | variable DIVASSIGN expression ';'
+                    | variable MULASSIGN expression ';'
     """
 
 
-def p_assignment(p):
-    """ assignment : assignable '=' instruction """
+
+def p_variable(p):
+    """ variable : ID
+                 | ID element_list
+    """
+
+def p_return(p):
+    """ return : RETURN expression ';' """
 
 
-def p_arithmetic_assignment(p):
-    """ arithmetic_assignment : assignable ADDASSIGN signed_number
-                              | assignable SUBASSIGN signed_number
-                              | assignable DIVASSIGN signed_number
-                              | assignable MULASSIGN signed_number
+def p_break(p):
+    """ break : BREAK ';' """
+
+
+def p_continue(p):
+    """ continue : CONTINUE ';' """
+
+def p_value_expression(p):
+    """ expression : INTNUM
+                   | STRING
+                   | FLOAT
+    """
+def p_id_expression(p):
+    """ expression : ID """
+
+
+def p_arithmetic_expression(p):
+    """ expression : expression ADD expression
+                   | expression SUB expression
+                   | expression DIV expression
+                   | expression MUL expression
     """
 
 
-def p_CONDITION(p):
-    """ CONDITION : instruction EQ operand
-                  | instruction NEQ operand
-                  | instruction LEQ operand
-                  | instruction GEQ operand
-                  | instruction LT operand
-                  | instruction GT operand
+def p_matrix_expression(p):
+    """ expression : expression DOTADD expression
+                   | expression DOTSUB expression
+                   | expression DOTMUL expression
+                   | expression DOTDIV expression
     """
 
 
-def p_operand(p):
-    """ operand : STRING
-                | arithmetic_assignment
-                | number_instruction
-                | SIGNED_NUMBER
-                | matrix_instruction
-                | ARRAY
-                | function
-                | assignment
-                | assignable
+def p_relational_expression(p):
+    """ expression : expression EQ expression
+                   | expression NEQ expression
+                   | expression GEQ expression
+                   | expression LEQ expression
+                   | expression GT expression
+                   | expression LT expression
     """
+
+
+def p_negation_expression(p):
+    """ expression : SUB expression %prec NEGATIVE """
+
+
+def p_transposition_expression(p):
+    """ expression : expression "'" """
+
+
+def p_functional_expression(p):
+    """ expression : ZEROS '(' expression ')'
+                   | ONES '(' expression ')'
+                   | EYE '(' expression ')'
+    """
+
+def p_list(p):
+    """ expression : '[' element_list ']' """
+
+
+def p_element_list(p):
+    """ element_list : expression ',' element_list
+                     | expression
+    """
+
+
+def p_print(p):
+    """ print : PRINT element_list ';'
+              | PRINT '"' element_list '"' ';'
+    """
+
+def p_if_instruction(p):
+    """ instruction : IF '(' condition ')' instruction %prec IF
+                      | IF '(' condition ')' instruction ELSE instruction"""
+
+
+def p_while_instruction(p):
+    """ instruction : WHILE '(' condition ')' instruction """
+
+
+def p_for_instruction(p):
+    """ instruction : FOR variable '=' expression ':' expression instruction """
+
+
+def p_compound_instruction(p):
+    """ instruction : '{' instructions '}' """
 
 
 def p_condition(p):
-    """ condition : CONDITION
-                  | assignable
-    """
-
-
-def p_while_loop(p):
-    """ while_loop : WHILE '(' condition ')' single_instruction """
-
-
-def p_for_loop(p):
-    """ for_loop : FOR ID '=' number ':' number single_instruction """
-
-
-def p_if(p):
-    """ if : IF '(' condition ')' single_instruction %prec NO_ELSE
-           | IF '(' condition ')' single_instruction ELSE single_instruction
-    """
-
-
-def p_block(p):
-    """ block : '{' instructions '}'
-              | '{' '}'
-    """
-
-
-def p_number_instruction(p):
-    """ number_instruction : signed_number ADD arithmetic_operand
-                           | signed_number SUB arithmetic_operand
-                           | signed_number MUL arithmetic_operand
-                           | signed_number DIV arithmetic_operand
-    """
-
-
-def p_arithmetic_operand(p):
-    """ arithmetic_operand : number_instruction
-                           | number
-    """
-
-
-def p_SIGNED_NUMBER(p):
-    """ SIGNED_NUMBER : SUB NUMBER
-                      | SUB assignable
-                      | NUMBER
-    """
-
-
-def p_signed_number(p):
-    """ signed_number : SUB number
-                      | number
-    """
-
-
-def p_matrix_instruction(p):
-    """ matrix_instruction : array DOTADD matrix_operand
-                           | array DOTSUB matrix_operand
-                           | array DOTMUL matrix_operand
-                           | array DOTDIV matrix_operand
-    """
-
-
-def p_matrix_operand(p):
-    """ matrix_operand : array
-                       | matrix_instruction
-    """
-
-
-def p_function(p):
-    """ function : ZEROS '(' intnum ')'
-                 | ONES '(' intnum ')'
-                 | EYE '(' intnum ')'
-    """
-
-
-def p_intnum(p):
-    """ intnum : INTNUM
-               | assignable
-    """
-
-
-def p_NUMBER(p):
-    """ NUMBER : INTNUM
-               | FLOAT
-    """
-
-
-def p_number(p):
-    """ number : INTNUM
-               | FLOAT
-               | assignable
-    """
-
-
-def p_array(p):
-    """ array : ARRAY
-              | assignable
-    """
-
-
-def p_ARRAY(p):
-    """ ARRAY : '[' element_list ']'
-              | '[' element_list ']' "'"
-              | assignable "'"
-    """
-
-
-def p_index(p):
-    """ index : '[' index_list ']' """
-
-
-def p_index_list(p):
-    """ index_list : index_list ',' intnum
-                   | intnum
-    """
-
-
-def p_assignable(p):
-    """ assignable : ID index
-                   | ID %prec NO_INDEX"""
+    """ condition : expression """
 
 
 parser = yacc.yacc()
